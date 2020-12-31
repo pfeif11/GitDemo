885c3c09e1b154aa004592251b9070be842b87e2
pfeif11 <pfeif11@vt.edu>
2020-12-31 16:30:54 -0500

    first commit

---
+++ src/Java/Fibonacci.java
@@ -0,0 +1,21 @@
+package Java;
+
+public class Fibonacci {
+	
+	public static void main(String[] args) {
+		
+		int a=0;
+		int b=1;
+		int c;
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println(a+" ");
+			c=a+b;
+			a=b;
+			b=c;
+			//wahsaiksdkjkldmfalka
+			//sfdfsdsf
+		}
+	}
+
+}
